0e3f2bc1c8b7a362c7126b23a30ad40690e107e6
Ilhan YAPICI <iyapici@wallix.com>
2024-01-30 17:01:46 +0100

    create modal component

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,26 @@
+<script setup lang="ts">
+import Modal from "./components/Modal.vue";
+</script>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      showModal: false,
+    };
+  },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal;
+    },
+  },
+};
+</script>
+
 <template>
   <h1>Hello there</h1>
+  <Modal v-if="showModal" title="Modal" @close="toggleModal">
+    good morning
+  </Modal>
+  <button @click="toggleModal">open modal</button>
 </template>
---
+++ src/components/Modal.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+defineProps(["title"]);
+</script>
+
+<script lang="ts">
+export default {
+  methods: {
+    closeModal() {
+      this.$emit("close");
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="backdrop" @click="closeModal">
+    <div class="modal">
+      <h2>{{ title }}</h2>
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+h2 {
+  margin-top: 0;
+}
+
+.modal {
+  width: 400px;
+  padding: 20px;
+  margin: 100px auto;
+  background: white;
+  border-radius: 10px;
+}
+
+.backdrop {
+  top: 0;
+  left: 0;
+  position: fixed;
+  background: rgba(0, 0, 0, 0.35);
+  width: 100%;
+  height: 100%;
+}
+</style>
